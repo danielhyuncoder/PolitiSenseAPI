3441daa57f3e13d92d547847392b53043daed897
Daniel H <166893525+danielhyuncoder@users.noreply.github.com>
2024-06-16 18:20:11 -0500

    Create training.py

---
+++ training.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+df=pd.read_csv("/kaggle/input/democratvsrepublicantweets/ExtractedTweets.csv")
+df.dropna(inplace=True)
+
+X=df['Tweet'].values
+
+y=[]
+for i in df['Party'].values:
+    if i =='Democrat':
+        y.append(0)
+    else:
+        y.append(1)
+
+tokenizer=Tokenizer()
+tokenizer.fit_on_texts(X)
+sequences=pad_sequences(tokenizer.texts_to_sequences(X), 50)
+
+model=tf.keras.models.Sequential()
+model.add(tf.keras.layers.Embedding(len(tokenizer.word_index)+1, 64))
+model.add(tf.keras.layers.LSTM(32, activation="relu"))
+model.add(tf.keras.layers.Dense(10))
+model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+model.compile(optimizer="RMSprop", loss="binary_crossentropy", metrics=['accuracy'])
+
+model.fit(sequences, np.array(y), epochs=4)
+
+# model saving
+
+model.save("politicpolarity.h5")
