a7a5ee16f7c6766550e7697c6f0dd48fa133a156
Daniel H <166893525+danielhyuncoder@users.noreply.github.com>
2024-06-16 18:15:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import tensorflow as tf
+from Tokenizer import tokenizer
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+model=tf.keras.models.load_model("politicpolarity.h5")
+
+class PredModel(BaseModel):
+    text: str
+
+tk=tokenizer("tokenizer.txt")
+
+@app.post("/predict/text")
+def predict_text(body: PredModel):
+    pred_data=np.array([tk.text_to_sequence(body.text)])
+    #print(model.predict(pred_data)[0])
+    return {"prediction": str(model.predict(pred_data)[0][0])}
